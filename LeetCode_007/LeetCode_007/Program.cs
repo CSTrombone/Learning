0d451b074dca54cf1a4946a2fb0e703a3ca93f1f
Austin Oden <tboneoden+github@gmail.com>
2024-05-09 18:14:44 -0500

    LeetCode P7 completed

---
+++ LeetCode_007/LeetCode_007/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Intrinsics;
+using System.Security.Cryptography.X509Certificates;
+
+
+//Prerequisites
+
+public class Learning
+{
+    public delegate void Print(int x); //Change for different display types as need
+
+    public static void Main(string[] args)
+    {
+        //Setup
+        Print show = Display;
+
+        int x = -2147483412;
+
+        //Result
+        show(Reverse(x));
+
+
+        //Solution
+        int Reverse(int x)
+        {
+            int result = 0;
+
+            int MIN = -2147483648;
+            int MAX = 2147483647;
+
+            while (x != 0)
+            {
+                int digit = x % 10;
+                x /= 10;
+
+                if (result > MAX / 10 || (result == MAX / 10 && digit >= MAX % 10))
+                    return 0;
+
+                if (result < MIN / 10 || (result == MIN / 10 && digit <= MIN % 10))
+                    return 0;
+
+                result = result * 10 + digit; 
+
+            }
+
+            return result;
+        }
+
+    }
+
+    //Flexible display method
+    public static void Display(int x)
+    {
+        Console.Write(x);
+    }
+}
